767136147a9a4be2aa145d6fac8086faa4f93605
Betsq9 <betsq9@gmail.com>
2020-11-02 12:14:44 +0200

    Added functionality:
    -Add selecting the folder where the screenshot will be saved
    
    -Add the ability to take a screenshot by pressing a key

---
+++ Screen Cast/FilesCreReadWri.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace Screen_Cast
+{
+    class FilesCreReadWri
+    {
+        string path = @"D:/path.txt";
+        public void Write(string pathToFile)
+        {
+            if (!File.Exists(path)) //Проверка файла на наличме
+                File.WriteAllText(path, pathToFile);
+            else
+            {
+                if (pathToFile != "")
+                {
+                    File.Delete(path); //Удаление файла для перезаписи
+                    File.AppendAllText(path, pathToFile); //Перезапись
+                }         
+            }            
+        }
+        public string Read()
+        {
+            string s;
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    s = sr.ReadToEnd();
+                }
+            return s;
+        }
+    }
+}
--- Screen Cast/Form1.cs
+++ Screen Cast/Form1.cs
@@ -16,5 +16,30 @@ namespace Screen_Cast
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string PathFileToSave;//Переменная для сохранение пути файла, чтобы записать в файл
+            //Выбор пути куда сохранять файлы
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK) { }
+            PathFileToSave = fbd.SelectedPath;
+
+            FilesCreReadWri f = new FilesCreReadWri(); //Создание экземпляра класса для записи пути 
+            f.Write(PathFileToSave);//Запись пути
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FilesCreReadWri f = new FilesCreReadWri();//Создание экземпляра класса для считывание файла
+            string FSave = f.Read();//Считывание файла
+
+
+            Bitmap printscreen = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            Graphics graphics = Graphics.FromImage(printscreen as Image);
+            graphics.CopyFromScreen(0, 0, 0, 0, printscreen.Size);
+            String SetDateTime = DateTime.Now.ToString("yyyy.MM.dd_HH-mm-ss");
+            printscreen.Save(FSave + "/" + SetDateTime + ".jpeg", System.Drawing.Imaging.ImageFormat.Jpeg);
+        }
     }
 }
